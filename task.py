5c0e02c58d671945718d280dfbf05b6380584e65
ArundhathiR <arundhathi.raja628@gmail.com>
2026-01-15 23:32:18 +0530

    Add TaskManager class to manage multiple tasks

--- task.py
+++ task.py
@@ -16,11 +16,42 @@ class Task:
         print(f"Status: {status}")   
         print("-"*20)  
 
+class TaskManager:
+    def __init__(self):
+        self.tasks=[]
+    
+    def add_task(self,task):
+        self.tasks.append(task)
+
+    def view_tasks(self):
+        if not self.tasks:
+            print("No Tasks Available")
+            return
+        for index,task in enumerate(self.tasks,start=1):
+            print(f"Task:{index}:")
+            task.display()
+
+    def complete_task(self,task_number):
+        if 1<=task_number<=len(self.tasks):
+            self.tasks[task_number-1].mark_completed()
+            print("Task marked as completed.")
+        else:
+            print("Invalid Task Number.")    
+
 task1=Task("Math Assignment","15 Jan","High")
 task2=Task("DSA Practice","Daily","Medium")   
 
-task1.display()
-task2.display()        
+manager=TaskManager()
 
-task1.mark_completed()
-task1.display()
+manager.add_task(task1)
+manager.add_task(task2)
+#task1.display()
+#task2.display()        
+manager.view_tasks()
+
+manager.complete_task(1)
+
+manager.view_tasks()
+
+#task1.mark_completed()
+#task1.display()
